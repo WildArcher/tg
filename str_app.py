63bce6e4cbd9a23d9d6ce88fee89d780983a0bbf
WildArcher <61867189+WildArcher@users.noreply.github.com>
2022-04-27 17:35:50 +0300

    Add files via upload

---
+++ str_app.py
@@ -0,0 +1,40 @@
+from telethon import TelegramClient, events, sync
+
+import configparser
+import json
+import asyncio
+from datetime import date, datetime
+
+from telethon import TelegramClient
+from telethon.errors import SessionPasswordNeededError
+from telethon.tl.functions.messages import (GetHistoryRequest)
+from telethon.tl.types import PeerChannel
+import streamlit 
+
+
+start_date = streamlit.date_input(
+     "start date")
+
+
+# config = configparser.ConfigParser()
+# config.read("config.ini")
+
+# # Setting configuration values
+# api_id = config['Telegram']['api_id']
+# api_hash = config['Telegram']['api_hash']
+
+# api_hash = str(api_hash)
+
+# phone = config['Telegram']['phone']
+# username = config['Telegram']['username']
+
+# channel_name = streamlit.text_input('print channel_name')
+
+# channel_name = input('print channel_name')
+
+# @client.on(events.NewMessage(chats=f'{channel_name}'))
+# async def my_event_handler(event):
+#     streamlit.write(event.raw_text)
+
+# client.start()
+# client.run_until_disconnected()
