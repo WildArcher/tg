0645747f4f36f331632bcb36345f75fb7dcf271a
WildArcher <61867189+WildArcher@users.noreply.github.com>
2022-04-28 11:54:32 +0300

    Update str_app.py

--- str_app.py
+++ str_app.py
@@ -1,42 +1,55 @@
 from telethon import TelegramClient, events, sync
-
+import pandas as pd
+import numpy as np
+from collections import Counter
+import nltk
+from nltk.corpus import stopwords
+import pymorphy2
+import re
+import warnings
+import datetime
 import configparser
 import json
 import asyncio
 from datetime import date, datetime
 
-from telethon import TelegramClient
-from telethon.errors import SessionPasswordNeededError
-from telethon.tl.functions.messages import (GetHistoryRequest)
-from telethon.tl.types import PeerChannel
+# from telethon import TelegramClient
+# from telethon.errors import SessionPasswordNeededError
+# from telethon.tl.functions.messages import (GetHistoryRequest)
+# from telethon.tl.types import PeerChannel
 import streamlit 
+from tg_parser_utils import text_preprocessing
+from pathlib import Path
+morph = pymorphy2.MorphAnalyzer()
 
 
 start_date = streamlit.date_input(
      "start date")
 
+DATA_PATH = './data'
 
-config = configparser.ConfigParser()
-config.read("config.ini")
+data_new = pd.read_json(DATA_PATH / 'channel_messages_markettwits_new.json')
 
-# Setting configuration values
-api_id = config['Telegram']['api_id']
-api_hash = config['Telegram']['api_hash']
+with open('stops_russian.txt', newline='\n', encoding='utf-8') as w:
+    words = w.readlines()
+    words = [line.rstrip() for line in words] 
+    
+    
+stop_words = list(set([word.lower() for word in words]))
 
-api_hash = str(api_hash)
+with open('stops_nltk.txt', newline='\n', encoding='utf-8') as w:
+    words = w.readlines()
+    words = [line.rstrip().replace("'", "").replace(",", "") for line in words] 
+    
+    
+stop_nltk = list(set([word.lower() for word in words]))
 
-phone = config['Telegram']['phone']
-username = config['Telegram']['username']
+stop_words = stop_words + stop_nltk
 
-channel_name = streamlit.text_input('print channel_name')
 
-channel_name = input('print channel_name')
+new_text = data_new['message'].values[0]
+new_clean_text = text_preprocessing(text=new_text, lemmatize=True, 
+                                              replacement=False, del_stop_words=True, no_connection=False, 
+                                              del_word_less_2_symbol=True, stop_words=stop_words)
 
-streamlit.write('OK')
-
-@client.on(events.NewMessage(chats=f'{channel_name}'))
-async def my_event_handler(event):
-    streamlit.write(event.raw_text)
-
-client.start()
-client.run_until_disconnected()
+st.write(new_clean_text)
