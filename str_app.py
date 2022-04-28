f84c9fdb77a3f536765ec7a4e51e10162ad7e152
WildArcher <61867189+WildArcher@users.noreply.github.com>
2022-04-28 14:10:47 +0300

    Update str_app.py

--- str_app.py
+++ str_app.py
@@ -51,12 +51,13 @@ st.write("Clean text: ", new_clean_text)
 texts_to_lda = list(data['clean_text'].apply(lambda x: list(x.split(' '))))
 common_dictionary = Dictionary(texts_to_lda)
 # new_text_themes = get_lda_themes(new_clean_text, common_dictionary=common_dictionary, lda=lda_model)
+lda_model =  models.LdaModel.load('lda.model')
 
 new_texts_to_lda = list(new_clean_text.split(' '))
 
 new_texts_to_lda = [common_dictionary.doc2bow(text) for text in [new_texts_to_lda]]
 themes = []
-for row in np.array(lda[new_texts_to_lda])[0]:
+for row in np.array(lda_model[new_texts_to_lda])[0]:
     if row[1] > 0.1:
         themes.append(row[0])
 
